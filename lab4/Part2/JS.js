9f3c32d9c32537744f334a3ef061a9a05785a67d
cmccorquodale-dev <c.mccorquodale01@gmail.com>
2026-07-28 15:06:57 -0400

    Part 2 done

---
+++ lab4/Part2/JS.js
@@ -0,0 +1,46 @@
+/*
+Name: Cailum McCorquodale
+File: index.html
+INFT1206 – Web Development Fundamentals
+Date: 28 July 2026
+Description: Image gallery example
+*/
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
+const images = [ { filename: 'pic1.jpg', alt: 'Closeup of a human eye' }, { filename: 'pic2.jpg', alt: 'Rock formation resembling a wave' }, { filename: 'pic3.jpg', alt: 'Purple and white pansies' }, { filename: 'pic4.jpg', alt: 'Section of an Egyptian wall painting' }, { filename: 'pic5.jpg', alt: 'Large butterfly on a leaf' }
+];
+const baseUrl = 'images/';
+for (const image of images) {
+    const newImage = document.createElement("img");
+    newImage.setAttribute("src", baseUrl + image.filename);
+    newImage.setAttribute("alt", image.alt);
+    newImage.setAttribute('tabindex', '0');
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener("click", () => {
+        updateDisplayedImage(image.filename, image.alt);
+    });
+    newImage.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+        updateDisplayedImage(e.target);
+        }
+    });
+}   
+function updateDisplayedImage(activeImage) {
+  displayedImage.setAttribute('src', activeImage.getAttribute('src'));
+  displayedImage.setAttribute('alt', activeImage.getAttribute('alt'));
+}
+btn.addEventListener('click', () => {
+  const isDark = btn.classList.contains('dark');
+
+  if (isDark) {
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgb(0 0 0 / 0.5)';
+  } else {
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgb(0 0 0 / 0)';
+  }
+    btn.classList.toggle('dark');
+});
