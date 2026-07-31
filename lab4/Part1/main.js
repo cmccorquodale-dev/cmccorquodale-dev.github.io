38ef611c4d35c8cc318a28421ddcaea80b7a638b
cmccorquodale-dev <c.mccorquodale01@gmail.com>
2026-07-30 23:36:58 -0400

    This?

--- lab4/Part1/main.js
+++ lab4/Part1/main.js
@@ -1,6 +1,6 @@
 /*
 Name: Cailum McCorquodale
-File:js.javascript
+File: main.js
 INFT1206 – Web Development Fundamentals
 Date: 28 July 2026
 Description: A random story generator
@@ -32,36 +32,31 @@ function randomValueFromArray(array) {
 // turned into a slug and slithered away
 
 // Partial return random string function
-const Character = randomValueFromArray(["Willy the Goblin", "Big Daddy", "Father Christmas"]);
-const Place = randomValueFromArray(["the soup kitchen", "Disneyland", "the White House"]);
-const Action = randomValueFromArray(["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"]);
-
 function returnRandomStoryString() {
-  const randomCharacter = randomValueFromArray(Character);
-  const randomPlace = randomValueFromArray(Place);
-  const randomEvent = randomValueFromArray(Action);
-  let storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
-return storyText;
+  const randomCharacter = randomValueFromArray(["Willy the Goblin", "Big Daddy", "Father Christmas"]);
+  const randomPlace = randomValueFromArray(["the soup kitchen", "Disneyland", "the White House"]);
+  const randomEvent = randomValueFromArray(["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"]);
+ 
+  let storyText = `It was 94 Fahrenheit outside, so Bob went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. ${randomCharacter} saw the whole thing, but was not surprised — Bob weighs 300 pounds, and it was a hot day.`;
+  return storyText;
 }
+ 
 generateBtn.addEventListener("click", generateStory);
-
-
+ 
 function generateStory() {
   let newStory = returnRandomStoryString();
   if (customName.value !== "") {
     const name = customName.value;
     newStory = newStory.replace("Bob", name);
-
   }
-
+ 
   if (document.getElementById("uk").checked) {
     const weight = Math.round(300/14) + " stone";
     const temperature = Math.round((94 - 32) * (5/9)) + " celsius";
     newStory = newStory.replace('300 pounds', weight);
-    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('94 Fahrenheit', temperature);
   }
-
-
+ 
   story.textContent = newStory;
   story.style.visibility = "visible";
 }
